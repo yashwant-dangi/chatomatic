c1da57c9ca00a5df0fb2dd86e265dbf76d60fa39
yashwant dangi <dangi.yd100@gmail.com>
2022-01-15 20:27:56 +0530

    added chat interface

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   useQuery,
+  useMutation,
   gql,
 } from "@apollo/client";
+
+import { Container, Row, Col, FormInput, Button } from "shards-react";
 
 const client = new ApolloClient({
   //   uri: "https://48p1r2roz4.sse.codesandbox.io",
@@ -29,15 +32,101 @@ const Messages = ({ user }) => {
   if (!data) {
     return null;
   } else {
-    return JSON.stringify(data);
+    return (
+      <>
+        {data.messages.map(({ id, user: messageUser, content }) => (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: user === messageUser ? "flex-end" : "flex-start",
+              paddingBottom: "1em",
+            }}
+          >
+            {user !== messageUser && (
+              <div
+                style={{
+                  height: 50,
+                  width: 50,
+                  marginRight: "0.5em",
+                  border: "2px solid #e5e6ea",
+                  borderRadius: 25,
+                  textAlign: "center",
+                  fontSize: "18pt",
+                  paddingTop: 5,
+                }}
+              >
+                {messageUser.slice(0, 2).toUpperCase()}
+              </div>
+            )}
+            <div
+              style={{
+                background: user === messageUser ? "#58bf56" : "#e5e6ea",
+                color: user === messageUser ? "white" : "black",
+                padding: "1em",
+                borderRadius: "1em",
+                maxWidth: "60%",
+              }}
+            >
+              {content}
+            </div>
+          </div>
+        ))}
+      </>
+    );
   }
 };
 
 const Chat = () => {
+  const [state, setState] = useState({
+    user: "Jack",
+    content: "",
+  });
+  const onSend = () => {
+    if (state.content.length > 0) {
+    }
+    setState({
+      ...state,
+      content: "",
+    });
+  };
   return (
-    <div>
-      <Messages user={"Jack"} />
-    </div>
+    <Container>
+      <Messages user={state.user} />
+      <Row>
+        <Col xs={2} style={{ padding: 0 }}>
+          <FormInput
+            label="User"
+            value={state.user}
+            onChange={(evt) => {
+              setState({
+                ...state,
+                user: evt.target.value,
+              });
+            }}
+          ></FormInput>
+        </Col>
+        <Col xs={8}>
+          <FormInput
+            label="Content"
+            value={state.content}
+            onChange={(evt) => {
+              setState({
+                ...state,
+                user: evt.target.value,
+              });
+            }}
+            onKeyUp={(evt) => {
+              if (evt.keyCode === 13) {
+                onSend();
+              }
+            }}
+          ></FormInput>
+        </Col>
+        <Col xs={2} style={{ padding: 0 }}>
+          <Button onClick={() => onSend()}>Send</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
