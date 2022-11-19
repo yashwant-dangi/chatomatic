11a76fc0b33110db54f2651fb8fc861d9197a74d
yashwant dangi <dangi.yd100@gmail.com>
2022-11-19 23:47:39 +0530

    optimization: one channel per one chat room

--- server/server.js
+++ server/server.js
@@ -34,15 +34,17 @@ const resolvers = {
         user,
         content,
       });
-      subscribers.forEach((fn) => fn());
+      // subscribers.forEach((fn) => fn());
+      pubsub.publish('mychannel', { messages })
       return id;
     },
   },
   Subscription: {
     messages: {
       subscribe: (parent, args, { pubsub }) => {
-        const channel = Math.random().toString(36).slice(2, 15);
-        onMessagesUpdates(() => pubsub.publish(channel, { messages }));
+        // const channel = Math.random().toString(36).slice(2, 15);
+        const channel = 'mychannel';
+        // onMessagesUpdates(() => pubsub.publish(channel, { messages }));
         setTimeout(() => pubsub.publish(channel, { messages }), 0);
         return pubsub.asyncIterator(channel);
       },
